5daedbabb52a93e278ef93828b98f6e714dd0244
SheersakSaha15 <sheersak.1506@gmail.com>
2025-09-22 12:10:00 +0530

    debugging js code

--- emailback.js
+++ emailback.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 5000;
-require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.json());
 app.post('/send-email', async (req, res) => {
